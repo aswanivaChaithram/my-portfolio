e433a8cad5c36ffbc76cb69bccc499d87ce730d5
aswanivaChaithram <aswaniva040@gmail.com>
2025-07-09 16:04:59 +0530

    modified

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,11 +9,6 @@ const Footer = () => {
       <div className="footer-info">
         <a href="/"><img src={logo} alt="" /></a>
         <p>© 2025 Aswani V A. All rights reserved.</p>
-        <div className="conditions">
-          <p>Term of Services</p>
-          <p>Privacy Policy</p>
-          <p>Connect with me</p>
-        </div>
       </div>
     </div>
   )
